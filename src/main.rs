b2ec83c4568b43fac40371f5f857511a8e9785ff
d3npa <kyo@tilde.pink>
2024-02-02 09:53:27 +0900

    add --tar flag feature

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,96 @@
-use std::fs;
+use std::io::Write;
+use std::path::Path;
 use std::result::Result;
+use std::{env, fs};
 use teleporter_factory::{ExitInfo, Exits};
 
 const EXITS_PATH: &str = "./exits";
 
+fn generate_stdout(exits: Exits) {
+    for exit in &*exits {
+        println!("\x1b[95m[*] '/etc/hostname.{}'\x1b[0m\n", exit.iface);
+        println!("{}", exit.gen_hostname_if());
+    }
+
+    println!("\x1b[95m[*] /etc/pf/teleport_hub.conf\x1b[0m\n");
+    println!("{}", exits.gen_pf_rules());
+
+    println!("\x1b[95m[*] /etc/teleport-rs/exits.toml\x1b[0m\n");
+    println!("{}", exits.gen_hub_config());
+}
+
+fn generate_tar(exits: Exits) {
+    let wrk_dir = "./tmp/teleporter_factory_wrkdir";
+
+    println!("[*] cleaning '{wrk_dir}'");
+    if fs::metadata(wrk_dir).is_ok() {
+        // if it exists
+        fs::remove_dir_all(wrk_dir).unwrap();
+    }
+
+    // creating files
+    {
+        /// short-hand to write-to-file lol
+        fn wtf(path: impl AsRef<Path>, contents: impl AsRef<[u8]>) {
+            let path = path.as_ref();
+            println!("[*] creating '{}'", path.display());
+            if let Ok(mut file) =
+                fs::OpenOptions::new().create(true).write(true).open(&path)
+            {
+                file.write_all(contents.as_ref())
+                    .expect(&format!("writing to {}", path.display()));
+            }
+        }
+
+        for sub_dir in vec!["", "etc", "etc/pf", "etc/teleport-rs"] {
+            let dir = format!("{wrk_dir}/{sub_dir}");
+            fs::create_dir_all(&dir)
+                .expect(&format!("creating directory {dir}"));
+        }
+
+        for exit in &*exits {
+            let path = format!("{wrk_dir}/etc/hostname.{}", exit.iface);
+            wtf(path, exit.gen_hostname_if());
+        }
+
+        let pf_rules = format!("{wrk_dir}/etc/pf/teleport_hub.conf");
+        wtf(pf_rules, exits.gen_pf_rules());
+
+        let exits_toml = format!("{wrk_dir}/etc/teleport-rs/exits.toml");
+        wtf(exits_toml, exits.gen_hub_config());
+    }
+
+    // creating archive
+    {
+        let archive_path = format!("{wrk_dir}/teleporters.tar");
+        let archive =
+            fs::File::create(&archive_path).expect("creating {archive_path}");
+
+        let mut tar_builder = tar::Builder::new(archive);
+        tar_builder
+            .append_dir_all("etc", format!("{wrk_dir}/etc"))
+            .unwrap();
+        tar_builder
+            .finish()
+            .expect(&format!("creating tar archive at {archive_path}"));
+
+        fs::copy(archive_path, "./archive.tar")
+            .expect("copying {archive_path} to cwd");
+    }
+}
+
 fn main() {
+    /* default behavior is to output configs to stdout to be copy-pasted.
+     * the --tar flag instead requests an extractable tar archive be prepared
+     */
+    let mut tar_flag = false;
+    let args: Vec<String> = env::args().collect();
+    if let Some(arg1) = args.get(1) {
+        if arg1 == "--tar" {
+            tar_flag = true;
+        }
+    }
+
     let entries = fs::read_dir(EXITS_PATH).unwrap();
 
     let mut exits = Exits::new();
@@ -23,14 +109,9 @@ fn main() {
 
     exits.sort_by(|a, b| a.iface.cmp(&b.iface));
 
-    for exit in &*exits {
-        println!("\x1b[95m[*] '/etc/hostname.{}'\x1b[0m\n", exit.iface);
-        println!("{}", exit.gen_hostname_if());
+    if tar_flag {
+        generate_tar(exits)
+    } else {
+        generate_stdout(exits)
     }
-
-    println!("\x1b[95m[*] /etc/pf/teleport_hub.conf\x1b[0m\n");
-    println!("{}", exits.gen_pf_rules());
-
-    println!("\x1b[95m[*] /etc/teleport-rs/exits.toml\x1b[0m\n");
-    println!("{}", exits.gen_hub_config());
 }
